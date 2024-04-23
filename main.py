b189a04b39c4bdd426090553b29a17d8193dcac8
WolfLatte <mho001@student.franciscan.edu>
2024-04-23 09:53:19 -0400

    added

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import tkinter as tk
+
+# Game Settings
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPACE = 50
+BODY = 1
+SNAKE_COLOR = "E7906D"
+FOOD = "#996DE7"
+BG = "E6D8D2"
+
+
+#Base funtions
+
+class Snake:
+    pass
+
+
+class Food:
+    pass
+
+
+def turn():
+    pass
+
+
+def change_direction(turn):
+    pass
+
+def game_over():
+    pass
+#######################################################
 window = tk.Tk()
 window.title("window")
 
-hi
-hello
-i like the sun!
 
-yay me too
+window.mainloop()
