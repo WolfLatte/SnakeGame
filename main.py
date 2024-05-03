e326d62a63bc7916d289a34e84c00adc8cdde360
WolfLatte <mho001@student.franciscan.edu>
2024-05-03 19:37:30 -0400

    added

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Snake:
         # Set up the game loop
         self.direction = 'Right'
         self.game_over = False
-        self.delay = 100
+        self.delay = 200
         self.score = 0
         #Written by Maite
         self.label = tk.Label(
